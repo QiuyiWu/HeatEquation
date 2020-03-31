c5635747724057521ff150fc999600dc963b5429
Qiuyi Wu <qiuyi_wu@urmc.rochester.edu>
2020-03-30 21:25:50 -0400

    Update PDE_functions.R

--- source/PDE_functions.R
+++ source/PDE_functions.R
@@ -3,7 +3,7 @@ Laplacian <- function(y_k, dx){
   diff(y_k)/dx  #dx = 0.1
 }
 
-HeatEqu <- function(y0, dx, g, dt, Nt){
+HeatEqn <- function(y0, dx, g, dt, Nt){
   yhat = array(NA, Nt)
   yhat[1] = y0
   for (i in 1:(Nt-1)) {
@@ -12,9 +12,9 @@ HeatEqu <- function(y0, dx, g, dt, Nt){
   return(yhat)
 }
 
-#---- Example ------
+#----- Example ------
 
 # x <- seq(-1,1,0.01)
 # y <- x^2 + rnorm(length(x), 0, 0.03) # real value  
-# HeatEqu(y0 = y[1], dx = 0.1, g = 1, dt = 0.01, Nt = 200)
+# HeatEqn(y0 = y[1], dx = 0.1, g = 1, dt = 0.01, Nt = 200)
 
