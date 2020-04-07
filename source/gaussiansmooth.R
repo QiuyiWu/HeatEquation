c68c3e195a9469ea5bc5d5e4f1eadf36d7bc7f31
cuongpham1995 <56059095+cuongpham1995@users.noreply.github.com>
2020-04-07 01:08:56 -0400

    Create gaussiansmooth.R

---
+++ source/gaussiansmooth.R
@@ -0,0 +1,24 @@
+
+#the function gauss.kernel takes two value: a vector y and an index i
+
+gauss.kernel = function(x, y,ind,b){
+  k = exp(-((x[ind] - x)^2)/(2*b))
+  yhat = sum(k*y)/sum(k)
+  return(yhat)
+}
+
+gaussian.smooth = function(x,y,b){
+  yhat = vector(length = length(y))
+  for(i in 1:length(y)){
+    yhat[i] = gauss.kernel(x,y,i,b)
+  }
+  return(yhat)
+}
+
+#example
+set.seed(1)
+x <- seq(-1,1,0.01)
+y <- x^2 + rnorm(length(x), 0, 1)
+plot(x,y)
+plot(x, gaussian.smooth(x,y,0.01))
+plot(x, gaussian.smooth(x,y,0.2))
