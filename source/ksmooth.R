e967c1bf04a445f9150aaf91b372595ee426b440
Qiuyi Wu <qiuyi_wu@urmc.rochester.edu>
2020-04-21 07:47:17 -0400

    Update ksmooth.R

--- source/ksmooth.R
+++ source/ksmooth.R
@@ -1,9 +1,9 @@
 # Example 
 set.seed(1)
-x <- seq(-1,1,0.01)
-y <- x^2 + rnorm(length(x), 0, 1) # real value  
-hq <- HeatEqn(y0 = y, dx = 0.001, g = 1, dt = 0.0000001, Nt = 3000, history = T)
+x <- seq(-1,1,0.1)
+y <- x^2 + rnorm(length(x), 0, 0.1) # real value  
+hq <- HeatEqn(y0 = y, dx = 0.1, g = 1, dt = 0.001, Nt = 275, history = F)
 tail(hq)
 plot(x, y)
-plot(x, hq[[2002]])
+plot(x, hq)
 plot(ksmooth(x, y, "normal",  bandwidth = 2))
